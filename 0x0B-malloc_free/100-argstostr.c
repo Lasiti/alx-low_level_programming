cb761c4b10537cf326b6c0cf397c1028e4ee331e
Festus Lasiti <festuslasiti@gmail.com>
2022-10-04 14:42:40 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,9 +22,10 @@ size++;
 size++;
 }
 size++;
-/* allocate memory for total number of chars and
- * new line for each word
- */
+/**
+  * allocate memory for total number of chars and
+  * new line for each word
+  **/
 strDup = malloc(sizeof(char) *size);
 if (strDup == NULL)
 return (NULL);
